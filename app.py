9a9c421ed7aa8051063a58b1396d012be0313c31
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-04-18 08:24:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="broker.hivemq.com"
+broker="64.92.183.45"
 port=1883
 client1= paho.Client("nataliaapp")
 client1.on_message = on_message
